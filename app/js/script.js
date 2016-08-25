3e1080ab4cbd00199607245f2c94ec55b08e8f22
Eddie Jimenez <casadebanho@gmail.com>
2016-08-25 00:38:11 -0700

    lots of code

--- app/js/script.js
+++ app/js/script.js
@@ -1,31 +1,75 @@
-var cubeSides = {
-  0: [4, 2, 5, 3],
-  1: [4, 3, 5, 2],
-  2: [4, 1, 5, 0],
-  3: [4, 0, 5, 1],
-  4: [1, 2, 0, 3],
-  5: [0, 2, 1, 3]
+// var cubeSides = {
+//   0: [4, 2, 5, 3],
+//   1: [4, 3, 5, 2],
+//   2: [4, 1, 5, 0],
+//   3: [4, 0, 5, 1],
+//   4: [1, 2, 0, 3],
+//   5: [0, 2, 1, 3]
+// };
+
+// var faces = {
+//   0: 'front',
+//   1: 'back',
+//   2: 'right',
+//   3: 'left',
+//   4: 'top',
+//   5: 'bottom'
+// };
+
+// var front = {
+
+// };
+
+var rotations = {
+  front: '',
+  back: '',
+  right: 'rotateX(270deg)',
+  left: '',
+  top: '',
+  bottom: ''
 };
 
-var faces = {
-  0: 'front',
-  1: 'back',
-  2: 'right',
-  3: 'left',
-  4: 'top',
-  5: 'bottom'
+var sides = {
+  front: ['top', 'right', 'bottom', 'left'],
+  back: ['top', 'left', 'bottom', 'right'],
+  right: ['top', 'back', 'bottom', 'front'],
+  left: ['top', 'front', 'bottom', 'back'],
+  top: ['back', 'right', 'front', 'left'],
+  bottom: ['front', 'right', 'back', 'left']
 };
+
+var orientation = {
+  front: 'z',
+  back: 'z',
+  right: 'x',
+  left: 'x',
+  top: 'y',
+  bottom: 'y'
+}
+
+// var orientation = ['x', 'y', 'z'];
+// var orientation = 'x';
 
 var App = function() {
   this.cube = document.querySelector('.cube');
   this.borders = document.querySelectorAll('.border');
-  this.currentFace = 0;
-  this.startTime = null;
-  this.selected = null;
+  this.currentFace = 'front';
+  // this.currentFace = 0;
+  // this.startTime = null;
+  // this.selected = null;
+
+  // camera
+  this.xRot = 0;
+  this.yRot = 0;
 
   this.borders.forEach(function(border, i) {
     this.handleClick(border, i);
-  }.bind(this))
+  }.bind(this));
+
+  // this.turnToFace(0);
+  // this.turnToFace(2);
+
+  this.cube.style.transform = rotations[this.currentFace];
 };
 
 App.prototype.constructor = App;
@@ -49,6 +93,102 @@ App.prototype.handleClick = function(border, i) {
 };
 
 App.prototype.turnToFace = function(i) {
+  var transform;
+
+
+  // (-?\d*)deg
+
+//   var re = /\w+\s/g;
+// var str = "fee fi fo fum";
+// var myArray = str.match(re);
+
+// debugger
+  var regex = /(\d*)deg/g;
+  var offset = rotations[this.currentFace].match(regex) || 0;
+
+
+  // while(this.xRot < 0) {
+  //   this.xRot += 360;
+  // }
+  // this.xRot %= 360;
+  // while(this.yRot < 0) {
+  //   this.yRot += 360;
+  // }
+  // this.yRot %= 360;
+
+
+  offset = parseInt(offset[0]) || 0;
+
+  var orient = orientation[this.currentFace]
+console.log("orient", orient, offset)
+// debugger
+
+
+  if(orient === 'x' && this.yRot === 90 && rotations[this.currentFace]) {
+    // debugger
+    offset = (180 + offset) % 360;
+  }
+  else if(orient === 'z') {}
+
+
+  offset = (offset / 90) % 4 || 0;
+  console.log("offset", offset)
+// debugger
+
+  var direction = (i - offset + 4) % 4;
+  // direction = i;
+
+  this.currentFace = sides[this.currentFace][direction];
+
+
+  console.log("current", this.currentFace, rotations[this.currentFace], orient)
+  // debugger
+
+  if(direction === 0) {
+    this.xRot += 90;
+    transform = 'rotateX(' + this.xRot + 'deg)';
+    this.yRot = 0;
+  }
+  else if(direction === 2) {
+    this.xRot -= 90;
+    transform = 'rotateX(' + this.xRot + 'deg)';
+    this.yRot = 0;
+  }
+  else if(direction === 1) {
+    this.yRot += 90;
+    transform = 'rotateY(' + this.yRot + 'deg)';
+    this.xRot = 0;
+  }
+  else if(direction === 3) {
+    this.yRot -= 90;
+    transform = 'rotateY(' + this.yRot + 'deg)';
+    this.xRot = 0;
+  }
+
+  if(Math.abs(this.xRot) === 180) {
+    transform = 'rotateY(180deg)';
+    this.xRot = 0;
+    this.yRot = 180;
+  }
+
+  transform += ' ' + rotations[this.currentFace];
+  this.cube.style.transform = transform;
+  // this.cube.style.transform = transform;
+
+  console.log("transform", transform)
+  console.log("yrot", this.yRot)
+
+  // setTimeout(function() {
+  //   transform += ' ' + rotations[this.currentFace];
+  //   this.cube.style.transform = transform;
+  // }.bind(this), 300);
+
+
+  // console.log("i", i, transform, rotations[this.currentFace])
+  // console.log("x", this.xRot)
+  // console.log("y", this.yRot)
+/*
+
   // debugger
   var oldFace = faces[this.currentFace];
   // this.currentFace = cubeSides[this.currentFace][i];
@@ -73,7 +213,7 @@ App.prototype.turnToFace = function(i) {
   }.bind(this), 1000);
 
   // console.log("timestamp", Math.floor(timestamp/1000))
-
+*/
 
 
   // var seconds = Math.floor(Date.now() / 1000);
